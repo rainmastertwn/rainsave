861c8aa309fe40a9de9075d40f9f995ab70c5207
Tim <tim.chao0825@gmail.com>
2025-08-13 10:11:43 +0800

    cicd: set gh-pages
    cicd: set gh-pages

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,15 +1,16 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="height-space">index page</div>
+  <div class="height-space">index page action 123</div>
 </template>
 
 <style lang="scss" scoped>
 .height-space {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
   // border: 2px solid red;
   height: 2000px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
 }
 </style>
